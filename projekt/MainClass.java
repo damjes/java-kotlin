e4aacd999f2efc8344c83b7bfb20648b5fa1186c
Damjes <git@damj.es>
2024-09-05 08:37:20 +0200

    Dodano kod w Kotlinie

--- projekt/MainClass.java
+++ projekt/MainClass.java
@@ -1,5 +1,10 @@
+package projekt;
+
+import projekt.Pomnoz;
+
 class MainClass {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println(Pomnoz.pomnoz(2, 3));
     }
 }
---
+++ projekt/Pomnoz.kt
@@ -0,0 +1,9 @@
+package projekt
+
+class Pomnoz {
+    companion object {
+        @JvmStatic fun pomnoz(a: Int, b: Int): Int {
+            return a * b
+        }
+    }
+}
